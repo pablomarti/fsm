3fd76cf8b11d494d14b7123898a7f6c34bf8061d
jhlive <kingblak_katanga@hotmail.com>
2013-03-01 05:20:30 -0600

    Arreglando problemas con la habilidad para la trabajadora social

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -23,7 +23,7 @@ class Ability
       if user.role? :edit_case
         can :read, HumanRecord
 
-        if user.role? :legal_attention
+        if user.role? :legal_atention
           can :give_legal_advice, HumanRecord
         end
 
